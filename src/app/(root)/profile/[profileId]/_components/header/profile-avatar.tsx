4f5c4d1c4eb5491cfcbaacb781a307943155a711
Vladimir <103734460+Leladatan@users.noreply.github.com>
2024-02-08 20:23:40 +0300

    bit fix status online

--- src/app/(root)/profile/[profileId]/_components/header/profile-avatar.tsx
+++ src/app/(root)/profile/[profileId]/_components/header/profile-avatar.tsx
@@ -23,7 +23,7 @@ const ProfileAvatar = ({user, actions, type = "default"}: {
 
   if (!isOwner) {
     return (
-      <>
+      <div className="relative">
         <Avatar className="w-48 h-48">
           <AvatarImage src={user.avatar || "/avatar.jpg"}/>
           <AvatarFallback><SkewLoader color="#36d7b7"/></AvatarFallback>
@@ -34,7 +34,7 @@ const ProfileAvatar = ({user, actions, type = "default"}: {
                 <div className="absolute bottom-4 right-4 w-5 h-5 bg-emerald-500 rounded-full animate-ping"/>
             </>
         }
-      </>
+      </div>
     );
   }
 
