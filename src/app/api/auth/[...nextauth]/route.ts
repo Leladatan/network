f376500415978cf1401d85a766469ab46b860323
Vladimir <103734460+Leladatan@users.noreply.github.com>
2024-05-27 15:54:28 +0300

    fix: fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,4 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import {db} from "@/lib/db";
-import bcrypt from "bcrypt";
-import {User} from "@prisma/client";
 import {authOptions} from "@/utils/constants/auth";
 
 const handler = NextAuth(authOptions);
--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -5,8 +5,9 @@ import bcrypt from "bcrypt";
 
 export async function POST(req: Request) {
   try {
+    // Получаем данные из формы регистрации
     const {email, username, password} = await req.json();
-
+    // Проверяем существует ли пользователь с такой электронной почтой
     const existsEmail: User | null = await db.user.findUnique({
       where: {
         email
@@ -16,7 +17,7 @@ export async function POST(req: Request) {
     if (existsEmail) {
       return NextResponse.json({message: "Email exist"}, {status: 400});
     }
-
+    // Проверяем существует ли пользователь с таким логином
     const existsUsername: User | null = await db.user.findUnique({
       where: {
         username
@@ -26,9 +27,9 @@ export async function POST(req: Request) {
     if (existsUsername) {
       return NextResponse.json({message: "Username exist"}, {status: 400});
     }
-
+    // Кешируем пароль
     const hashedPassword: string = await bcrypt.hash(password, 10);
-
+    // Создаем пользователя
     await db.user.create({
       data: {
         email,
